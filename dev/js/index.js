50411893104385722f02ec074cbd421af0fd46c6
Alvaro Felipe <alvaro@ed.team>
2018-08-27 16:45:56 -0500

    importa countdown

--- dev/js/index.js
+++ dev/js/index.js
@@ -2,9 +2,12 @@ import {saludo, despedida} from "./modules/example"
 import {activeMenu} from './modules/active-menu'
 import {showMenu} from "./modules/show-menu"
 import {scrollTarget} from './modules/scroll-target'
-import {timer} from './modules/countdown'
+import {countdown} from './modules/countdown'
 
-window.countdown = timer.run
+// window.countdown = countdown.run
 activeMenu()
 showMenu()
 scrollTarget()
+
+
+countdown.run('Mon Aug 27 2018 23:59:59 GMT-0500', 'clock', 'El descuento ha terminado')
